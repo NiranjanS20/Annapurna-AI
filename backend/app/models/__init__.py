ca6dbcf17a078ee4f21903643b38e59a083c8599
Niranjan Shanbhag <nshanbhag345@gmail.com>
2026-04-30 13:09:25 +0530

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,22 @@ from .food_data import FoodData
 from .prediction import Prediction
 from .alert import Alert
 from .donation import Donation
+from .donation_listing import DonationListing
+from .donation_acceptance import DonationAcceptance
+from .donation_notification import DonationNotification
+from .donation_audit_log import DonationAuditLog
+from .ngo_profile import NgoProfile
 
-__all__ = ['User', 'MenuItem', 'FoodData', 'Prediction', 'Alert', 'Donation']
+__all__ = [
+	'User',
+	'MenuItem',
+	'FoodData',
+	'Prediction',
+	'Alert',
+	'Donation',
+	'DonationListing',
+	'DonationAcceptance',
+	'DonationNotification',
+	'DonationAuditLog',
+	'NgoProfile',
+]
